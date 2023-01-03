291755ef38ad7ed4b594191516a1600c4b62ae69
Aaron White <w531t4@gmail.com>
2023-01-03 01:36:12 -0500

    update icecube project dir

---
+++ main_pll_inst.v
@@ -0,0 +1,4 @@
+main_pll main_pll_inst(.PACKAGEPIN(),
+                       .PLLOUTCORE(),
+                       .PLLOUTGLOBAL(),
+                       .RESET());
---
+++ synlog.tcl
@@ -0,0 +1,1 @@
+project -load template_syn.prj
