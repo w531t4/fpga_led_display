f9dab4217fba0f18befbf2b6100322bf40f66dfa
Aaron White <w531t4@gmail.com>
2023-01-03 08:36:13 -0500

    update deploy scripts

--- maketag.sh
+++ maketag.sh
@@ -2,17 +2,17 @@
 
 NAME=$1
 
-
-
 DATE=`date +%s`
-SRC_PATH=/home/awhite/CLionProjects/led_display/fpga/template_Implmnt/sbt/outputs/bitmap
+BASE_PATH=/home/awhite/Documents/Projects/fpga_led_display
+SRC_PATH=${BASE_PATH}/template_Implmnt/sbt/outputs/bitmap
 SRC_FILE=main_bitmap.bin
 
-DEST_PATH=/home/awhite/CLionProjects/led_display/fpga/bitmaps
-#tinyprog --program /home/awhite/CLionProjects/led_display/fpga/template_Implmnt/sbt/outputs/bitmap/main_bitmap.bin
-cp -f ${SRC_PATH}/${SRC_FILE} ${DEST_PATH}/main-${DATE}.bin
-cp -f ${SRC_PATH}/${SRC_FILE} ${DEST_PATH}/tag-${NAME}.bin
+DEST_PATH=${BASE_PATH}/bitmaps
 
-
-
-
+if [ "x$1" == "x" ]; then
+    cp -f ${SRC_PATH}/${SRC_FILE} ${DEST_PATH}/${DATE}-main.bin
+    echo "${DEST_PATH}/${DATE}-main.bin"
+else
+    cp -f ${SRC_PATH}/${SRC_FILE} ${DEST_PATH}/${DATE}-${NAME}-main.bin
+    echo "${DEST_PATH}/${DATE}-${NAME}-main.bin"
+fi
