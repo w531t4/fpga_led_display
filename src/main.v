c8bd5c37cbf7afe0ed6f0bef73608b082de2382b
Aaron White <w531t4@gmail.com>
2023-01-08 16:50:20 -0500

    persist alternative ram stuff jic

--- src/main.v
+++ src/main.v
@@ -250,6 +250,23 @@ fm6126init do_init (
 
 	) /* synthesis syn_noprune=1 */ ;
 
+/*
+	newram2 fb (
+		.PortAClk(clk_root),
+		.PortAAddr(ram_a_address),
+		.PortADataIn(ram_a_data_in),
+		.PortAWriteEnable(ram_a_write_enable),
+		.PortAReset(global_reset),
+		.PortBClk(clk_root),
+		.PortBDataIn(16'b0),
+		.PortBAddr(ram_b_address),
+		.PortBWriteEnable(1'b0),
+		.PortBReset(ram_b_reset),
+		.PortADataOut(ram_a_data_out),
+		.PortBDataOut(ram_b_data_out),
+		.PortAClkEnable(ram_a_clk_enable),
+		.PortBClkEnable(ram_b_clk_enable)
+	) */ /* synthesis syn_noprune=1 */ //;
 	multimem fb (
 		.ClockA(clk_root),
 		.AddressA(ram_a_address),
