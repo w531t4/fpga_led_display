69f0a0ea91b28288cfa677bbb1790092a3c333da
Aaron White <w531t4@gmail.com>
2023-01-11 15:43:32 +0000

    update uart_write_image tooling

--- uart_write_image.py
+++ uart_write_image.py
@@ -3,15 +3,11 @@ from uart_write import reset_state
 
 def main():
     serial_device = "/dev/ttyAMA0"
-    targetfile = "../mem/alphabet.mem"
+    targetfile = "../uart/alphabet.uart"
     baudrate=2444444
     ser = serial.Serial(serial_device, baudrate)
-    fw = open(targetfile, 'rb').read().split("\n")
-    while True:
-        current_row = 0
-        while current_row < 32:
-            ser.write(b"L" + b"".join(fw[(current_row*64):(current_row*64) + 64]))
-            current_row += 1
+    fw = open(targetfile, 'rb').read()
+    ser.write(fw)
     ser.close()
 
 if (__name__ == "__main__"):
