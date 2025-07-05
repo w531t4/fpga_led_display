a555fe03f09c634fc7b1fa805c85b17707e7a687
Aaron White <w531t4@gmail.com>
2025-07-05 13:47:41 -0400

    purge of syn/ice40 stuff

--- deploytag.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-NAME="$(/home/awhite/Documents/Projects/fpga_led_display/maketag.sh)"
-ssh fpga "mkdir -p /root/bitmaps"
-scp ${NAME} fpga:/root/bitmaps
-BASE=$(basename ${NAME})
-ssh fpga "/root/fpga_led_display/venv/bin/tinyprog --program /root/bitmaps/${BASE}"
-
