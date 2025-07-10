6ed67c5997ec8d766056bdf41c8544f364f5968f
Aaron White <w531t4@gmail.com>
2025-07-10 11:37:08 -0400

    remove globals from parse_lint

--- src/scripts/parse_lint.py
+++ src/scripts/parse_lint.py
@@ -1,79 +1,118 @@
 """ Parse linting output from verilator, and emit any items failing to match a denylist filter"""
 import sys
-from typing import List
+from typing import List, NamedTuple
+from pathlib import Path
 
-KEYWORD = "error"  # <-- Change this to whatever string you want to filter by
 
-in_grouping = False
-suppress_current_group = False
-grouping_text = list()
-issue_summary = list()
-issue_items = list()
-has_started = False
+class LintTitle(NamedTuple):
+    """holder of components of lint title line"""
+    severity: str
+    category: str
+    file: str
+    row: int
+    col: int
+
+def get_filter_items(path: Path) -> List[str]:
+    """ Get list of files to filter from config file"""
+    try:
+        return [x for x in path.read_text().split("\n") if len(x) > 0]
+    except FileNotFoundError:
+        return []
 
 def print_grouping(data: List[str]) -> None:
     """emit all items in the group"""
     for each in data:
         print(each)
-    issue_items.append(data)
 
-def is_suppressed(line_from_file: str) -> bool:
+def parse_lint_title(line: str) -> LintTitle:
+    """ parse row into an object"""
+    # selects Warning-UNUSEDSIGNAL
+    atype_param = line[1:].split(": ")[0]
+    atype, param = atype_param.split("-")
+
+    # selects src/main.v:147:16
+    file_row_col = line[1:].split(": ")[1]
+    file,row,col = file_row_col.split(":") # pylint: disable=unused-variable
+    return LintTitle(severity=atype,
+                     category=param,
+                     file=file,
+                     row=int(row),
+                     col=int(col),
+                     )
+
+def is_suppressed(line_from_file: str, filter_files: List[str]) -> bool:
     """determine if suppression should occur (based on components from the header line)"""
-    try:
-        atype_param = line_from_file[1:].split(": ")[0]
-        file_row_col = line_from_file[1:].split(": ")[1]
-        file,row,col = file_row_col.split(":") # pylint: disable=unused-variable
-        atype, param = atype_param.split("-")
-    except IndexError:
-        return False
+    # Example
+    #  '%Warning-UNUSEDSIGNAL: src/main.v:147:16: Signal is not used: 'num_commands_processed''
+    obj = parse_lint_title(line_from_file)
+    return obj.file in filter_files
 
-    filter_files = ["src/platform/tiny_ecp5_sim.v","src/platform/tiny_cells_sim.v", "src/new_pll.v"]
-    if not file in filter_files:
-        issue_summary.append((atype, param, file,))
-    return file in filter_files
-
-def print_summary(summary: List[str]) -> None:
+def print_summary(summary: List[LintTitle]) -> None:
     """emit a summary of the observed issues"""
-    summary_set = set(summary)
+    set()
+    summary_set = set([(x.severity, x.category, x.file) for x in summary])
+    print(summary_set)
     for each in sorted(list(summary_set), key=lambda x: x[2]):
-        print(f"{len([x for x in summary if x == each])}: {each[2]} {each[0]} {each[1]}")
+        # atype, param, file
+        print(f"{len([x for x in summary if (x.severity, x.category, x.file) == each[0:3]])}: "
+              f"{each[2]} {each[0]} {each[1]}")
 
+def main() -> None:
+    """ main """
+    files_to_filter = get_filter_items(Path(".verilator_lint"))
+    has_started = False
+    in_grouping = False
+    suppress_current_group = False
 
-for line in sys.stdin:
-    line = line.rstrip()
-    if not has_started and len(line) > 0 and line[0] == "%":
-        has_started = True
-    elif not has_started:
-        print(line)
-        continue
-    # print(f"mine line={line}")
-    if "%Error: Exiting due to" in line and "warning(s)" in line:
-        continue
-    if len(line) > 0 and line[0] == "%":
-        has_started = True
-        if in_grouping and not suppress_current_group:
-            print_grouping(grouping_text)
-            grouping_text = list()
-            in_grouping = False
-        # elif in_grouping and suppress_current_group:
-        #     suppress_current_group = False
-        in_grouping = True
-        suppress_current_group = is_suppressed(line)
-        if not suppress_current_group:
-            grouping_text.append(line)
-    elif in_grouping and suppress_current_group:
-        continue
-    elif in_grouping and not suppress_current_group:
-        grouping_text.append(line)
-
-if in_grouping and len(grouping_text) > 0 and not suppress_current_group:
-    print_grouping(grouping_text)
+    issue_summary: List[LintTitle] = list()
     grouping_text = list()
+    issue_items = list()
 
+    for line in sys.stdin:
+        line = line.rstrip()
 
-print_summary(issue_summary)
-if len(issue_items) > 0:
-    print(f"%Error: Exiting due to {len(issue_items)} warning(s)")
-    # sys.exit(1)
+        # Emit lines that verilator emits preceeding the lint output section
+        if not has_started and len(line) > 0 and line[0] == "%":
+            has_started = True
+        elif not has_started:
+            print(line)
+            continue
 
-sys.exit(0)
+        # verilator prints a summary by default - don't emit it, as the output
+        # doesn't take into account our filtering.
+        if "%Error: Exiting due to" in line and "warning(s)" in line:
+            continue
+
+        if len(line) > 0 and line[0] == "%":
+            if in_grouping and not suppress_current_group:
+                print_grouping(grouping_text)
+                issue_items.append(grouping_text)
+                grouping_text = list()
+                in_grouping = False
+            # elif in_grouping and suppress_current_group:
+            #     suppress_current_group = False
+            in_grouping = True
+            title_obj = parse_lint_title(line)
+            suppress_current_group = title_obj.file in files_to_filter
+            if not suppress_current_group:
+                grouping_text.append(line)
+                issue_summary.append(title_obj)
+        elif in_grouping and suppress_current_group:
+            continue
+        elif in_grouping and not suppress_current_group:
+            grouping_text.append(line)
+
+    if in_grouping and len(grouping_text) > 0 and not suppress_current_group:
+        print_grouping(grouping_text)
+        issue_items.append(grouping_text)
+        grouping_text = list()
+
+    print_summary(issue_summary)
+    if len(issue_items) > 0:
+        print(f"%Error: Exiting due to {len(issue_items)} warning(s)")
+        # sys.exit(1)
+
+    sys.exit(0)
+
+if __name__ == "__main__":
+    main()
