1fe18817c7761cf791289282fd2d689effb48956
Aaron White <w531t4@gmail.com>
2025-12-28 01:03:32 +0000

    fix: improperly formatted timescale/default_nettypes https://github.com/chipsalliance/verible/issues/986

--- src/testbenches/tb_brightness_timeout.sv
+++ src/testbenches/tb_brightness_timeout.sv
@@ -1,6 +1,9 @@
 // SPDX-FileCopyrightText: 2025 Aaron White <w531t4@gmail.com>
 // SPDX-License-Identifier: MIT
-`timescale 1ns / 1ns `default_nettype none
+// verilog_format: off
+`timescale 1ns / 1ns
+`default_nettype none
+// verilog_format: on
 module tb_brightness_timeout;
 
     logic clk;
