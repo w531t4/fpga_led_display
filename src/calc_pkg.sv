294be0d7e51274fae4fb863f74ad7e8637d588a6
Aaron White <w531t4@gmail.com>
2025-12-28 20:42:22 +0000

    fix: yosys parser doesn't support return statements..

--- src/calc_pkg.sv
+++ src/calc_pkg.sv
@@ -2,58 +2,59 @@
 // SPDX-License-Identifier: MIT
 package calc_pkg;
     function automatic int unsigned num_subpanels(input int unsigned pixel_height, input int unsigned pixel_halfheight);
-        return pixel_height / pixel_halfheight;
+        num_subpanels = pixel_height / pixel_halfheight;
     endfunction
 
     function automatic int unsigned num_subpanelselect_bits(input int unsigned pixel_height,
                                                             input int unsigned pixel_halfheight);
-        return $clog2(num_subpanels(pixel_height, pixel_halfheight));
+        num_subpanelselect_bits = $clog2(num_subpanels(pixel_height, pixel_halfheight));
     endfunction
 
     function automatic int unsigned num_pixelcolorselect_bits(input int unsigned bytes_per_pixel);
-        return $clog2(bytes_per_pixel);
+        num_pixelcolorselect_bits = $clog2(bytes_per_pixel);
     endfunction
 
     function automatic int unsigned num_column_address_bits(input int unsigned pixel_width);
-        return $clog2(pixel_width);
+        num_column_address_bits = $clog2(pixel_width);
     endfunction
 
     function automatic int unsigned num_row_address_bits(input int unsigned pixel_height);
-        return $clog2(pixel_height);
+        num_row_address_bits = $clog2(pixel_height);
     endfunction
 
     function automatic int unsigned num_address_b_bits(input int unsigned pixel_width,
                                                        input int unsigned pixel_halfheight);
-        return $clog2(pixel_halfheight) + num_column_address_bits(pixel_width);
+        num_address_b_bits = $clog2(pixel_halfheight) + num_column_address_bits(pixel_width);
     endfunction
 
     function automatic int unsigned num_address_a_bits(input int unsigned pixel_width, input int unsigned pixel_height,
                                                        input int unsigned bytes_per_pixel,
                                                        input int unsigned pixel_halfheight);
-        return num_subpanelselect_bits(pixel_height, pixel_halfheight) + num_pixelcolorselect_bits(bytes_per_pixel) +
-            num_address_b_bits(pixel_width, pixel_halfheight);
+        num_address_a_bits = num_subpanelselect_bits(pixel_height, pixel_halfheight)
+            + num_pixelcolorselect_bits(bytes_per_pixel)
+            + num_address_b_bits(pixel_width, pixel_halfheight);
     endfunction
 
     function automatic int unsigned num_structure_bits(input int unsigned pixel_width, input int unsigned pixel_height,
                                                        input int unsigned bytes_per_pixel,
                                                        input int unsigned pixel_halfheight);
-        return num_address_a_bits(pixel_width, pixel_height, bytes_per_pixel, pixel_halfheight) -
-            num_address_b_bits(pixel_width, pixel_halfheight);
+        num_structure_bits = num_address_a_bits(pixel_width, pixel_height, bytes_per_pixel, pixel_halfheight)
+            - num_address_b_bits(pixel_width, pixel_halfheight);
     endfunction
 
     function automatic int unsigned num_data_a_bits(input int unsigned data_a_bits);
-        return data_a_bits;
+        num_data_a_bits = data_a_bits;
     endfunction
 
     function automatic int unsigned num_data_b_bits(input int unsigned pixel_height, input int unsigned bytes_per_pixel,
                                                     input int unsigned pixel_halfheight);
-        return ((1 << num_pixelcolorselect_bits(bytes_per_pixel)) <<
-                num_subpanelselect_bits(pixel_height, pixel_halfheight)) << $clog2(8);
+        num_data_b_bits = ((1 << num_pixelcolorselect_bits(bytes_per_pixel))
+            << num_subpanelselect_bits(pixel_height, pixel_halfheight)) << $clog2(8);
     endfunction
 
     function automatic int unsigned num_bits_per_subpanel(
         input int unsigned pixel_height, input int unsigned bytes_per_pixel, input int unsigned pixel_halfheight);
-        return num_data_b_bits(pixel_height, bytes_per_pixel, pixel_halfheight) >>
-            num_subpanelselect_bits(pixel_height, pixel_halfheight);
+        num_bits_per_subpanel = num_data_b_bits(pixel_height, bytes_per_pixel, pixel_halfheight)
+            >> num_subpanelselect_bits(pixel_height, pixel_halfheight);
     endfunction
 endpackage
