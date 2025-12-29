eea6af845f9f716b621968790faef2a4811e86b9
Aaron White <w531t4@gmail.com>
2025-12-29 03:45:58 +0000

    parameterize control_cmd_watchdog

--- src/control_cmd_watchdog.sv
+++ src/control_cmd_watchdog.sv
@@ -2,7 +2,12 @@
 // SPDX-License-Identifier: MIT
 `default_nettype none
 module control_cmd_watchdog #(
-    parameter int unsigned WATCHDOG_CONTROL_TICKS = params_pkg::WATCHDOG_CONTROL_TICKS
+    parameter integer unsigned WATCHDOG_SIGNATURE_BITS = params_pkg::WATCHDOG_SIGNATURE_BITS,
+    parameter logic [WATCHDOG_SIGNATURE_BITS-1:0] WATCHDOG_SIGNATURE_PATTERN = params_pkg::WATCHDOG_SIGNATURE_PATTERN,
+    parameter int unsigned WATCHDOG_CONTROL_TICKS = params_pkg::WATCHDOG_CONTROL_TICKS,
+    // verilator lint_off UNUSEDPARAM
+    parameter integer unsigned _UNUSED = 0
+    // verilator lint_on UNUSEDPARAM
 ) (
     // input cmd_enable,
     input reset,
@@ -13,19 +18,19 @@ module control_cmd_watchdog #(
     output logic sys_reset,
     output logic done
 );
-    localparam WATCHDOG_SIGBYTES = $rtoi(params_pkg::WATCHDOG_SIGNATURE_BITS / 8);
+    localparam WATCHDOG_SIGBYTES = $rtoi(WATCHDOG_SIGNATURE_BITS / 8);
     typedef enum {
         STATE_SIG_CAPTURE,
         STATE_DONE
     } ctrl_fsm;
     ctrl_fsm state;
-    logic [params_pkg::WATCHDOG_SIGNATURE_BITS-1:0] cache;
+    logic [WATCHDOG_SIGNATURE_BITS-1:0] cache;
     logic [$clog2(WATCHDOG_CONTROL_TICKS)-1:0] watchdog_counter;
     logic [$clog2(WATCHDOG_SIGBYTES)-1:0] sig_byte_counter;
 
     always @(posedge clk) begin
         if (reset) begin
-            cache <= {params_pkg::WATCHDOG_SIGNATURE_BITS{1'b0}};
+            cache <= {WATCHDOG_SIGNATURE_BITS{1'b0}};
             watchdog_counter <= ($clog2(WATCHDOG_CONTROL_TICKS))'(WATCHDOG_CONTROL_TICKS);
             sig_byte_counter <= ($clog2(WATCHDOG_SIGBYTES))'(WATCHDOG_SIGBYTES);
             state <= STATE_SIG_CAPTURE;
@@ -37,9 +42,8 @@ module control_cmd_watchdog #(
                 STATE_SIG_CAPTURE: begin
                     if (enable) begin
                         // Update memory
-                        cache <= (cache << 8) + (params_pkg::WATCHDOG_SIGNATURE_BITS)'(data_in);
-                        if (((cache << 8) + (params_pkg::WATCHDOG_SIGNATURE_BITS)'(data_in)) ==
-                            params_pkg::WATCHDOG_SIGNATURE_PATTERN) begin
+                        cache <= (cache << 8) + (WATCHDOG_SIGNATURE_BITS)'(data_in);
+                        if (((cache << 8) + (WATCHDOG_SIGNATURE_BITS)'(data_in)) == WATCHDOG_SIGNATURE_PATTERN) begin
                             watchdog_counter <= ($clog2(WATCHDOG_CONTROL_TICKS))'(WATCHDOG_CONTROL_TICKS);
                         end else begin
                             watchdog_counter <= watchdog_counter - 'd1;
@@ -58,7 +62,7 @@ module control_cmd_watchdog #(
                 STATE_DONE: begin
                     state <= STATE_SIG_CAPTURE;
                     done <= 1'b0;
-                    cache <= {params_pkg::WATCHDOG_SIGNATURE_BITS{1'b0}};
+                    cache <= {WATCHDOG_SIGNATURE_BITS{1'b0}};
                     sig_byte_counter <= ($clog2(WATCHDOG_SIGBYTES))'(WATCHDOG_SIGBYTES);
                     watchdog_counter <= watchdog_counter - 'd1;
                 end
