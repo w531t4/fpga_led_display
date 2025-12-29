b77ec8bb433cd1a025121bb7a906a446ce41a3ca
Aaron White <w531t4@gmail.com>
2025-12-29 01:14:49 +0000

    parameterize multimem

--- src/multimem.sv
+++ src/multimem.sv
@@ -3,18 +3,23 @@
 // multimem.sv
 `default_nettype none
 module multimem #(
-    `include "memory_calcs.vh"
+    parameter integer unsigned BYTES_PER_PIXEL = params_pkg::BYTES_PER_PIXEL,
+    parameter integer unsigned PIXEL_HEIGHT = params_pkg::PIXEL_HEIGHT,
+    parameter integer unsigned PIXEL_WIDTH = params_pkg::PIXEL_WIDTH,
+    parameter integer unsigned PIXEL_HALFHEIGHT = params_pkg::PIXEL_HALFHEIGHT,
     // verilator lint_off UNUSEDPARAM
     parameter integer unsigned _UNUSED = 0
     // verilator lint_on UNUSEDPARAM
 ) (
-    input wire [_NUM_DATA_A_BITS-1:0] DataInA,
+    input wire [calc_pkg::num_data_a_bits()-1:0] DataInA,
     input wire [15:0] DataInB,
     // 12 bits [11:0]      -5-                   -log( (64*2),2)=7-
-    // input wire [$clog2(params_pkg::PIXEL_HEIGHT * params_pkg::PIXEL_WIDTH * params_pkg::BYTES_PER_PIXEL)-1:0] AddressA,
-    input wire [_NUM_ADDRESS_A_BITS-1:0] AddressA,
+    // input wire [$clog2(PIXEL_HEIGHT * PIXEL_WIDTH * BYTES_PER_PIXEL)-1:0] AddressA,
+    input wire [calc_pkg::num_address_a_bits(
+PIXEL_WIDTH, PIXEL_HEIGHT, BYTES_PER_PIXEL, PIXEL_HALFHEIGHT
+)-1:0] AddressA,
     // 11 bits [10:0] (-2, because this is 16bit, not 8bit), -3 because we're not pulling half panels anymore
-    input wire [_NUM_ADDRESS_B_BITS-1:0] AddressB,
+    input wire [calc_pkg::num_address_b_bits(PIXEL_WIDTH, PIXEL_HALFHEIGHT)-1:0] AddressB,
     input wire ClockA,
     input wire ClockB,
     input wire ClockEnA,
@@ -23,8 +28,8 @@ module multimem #(
     input wire WrB,
     input wire ResetA,
     input wire ResetB,
-    output wire [_NUM_DATA_A_BITS-1:0] QA,
-    output wire [_NUM_DATA_B_BITS-1:0] QB
+    output wire [calc_pkg::num_data_a_bits()-1:0] QA,
+    output wire [calc_pkg::num_data_b_bits(PIXEL_HEIGHT, BYTES_PER_PIXEL, PIXEL_HALFHEIGHT)-1:0] QB
 );
     // consider
     // 8 bits addr a, 2 bits colorselect, 2 bits display
@@ -42,30 +47,47 @@ module multimem #(
     //      0 pixelcolorselect = 0
     //  [7:0] mem [displaybits,colorselectbits] [addr_b bits]
 
-    localparam LANES = (1 << _NUM_STRUCTURE_BITS);
-    wire [LANES*_NUM_DATA_A_BITS-1:0] qb_lanes_w;
+    localparam LANES = (1 << calc_pkg::num_structure_bits(
+        PIXEL_WIDTH, PIXEL_HEIGHT, BYTES_PER_PIXEL, PIXEL_HALFHEIGHT
+    ));
+    wire [LANES*calc_pkg::num_data_a_bits()-1:0] qb_lanes_w;
 
     genvar i;
     generate
         for (i = 0; i < LANES; i = i + 1) begin : G
-            wire [_NUM_STRUCTURE_BITS-1:0] lane_idx_from_addr = {
-                AddressA[_NUM_ADDRESS_A_BITS-1-:_NUM_SUBPANELSELECT_BITS], AddressA[_NUM_PIXELCOLORSELECT_BITS-1:0]
+            wire [calc_pkg::num_structure_bits(
+PIXEL_WIDTH, PIXEL_HEIGHT, BYTES_PER_PIXEL, PIXEL_HALFHEIGHT
+)-1:0] lane_idx_from_addr = {
+                AddressA[calc_pkg::num_address_a_bits(
+                    PIXEL_WIDTH, PIXEL_HEIGHT, BYTES_PER_PIXEL, PIXEL_HALFHEIGHT
+                )-1-:calc_pkg::num_subpanelselect_bits(
+                    PIXEL_HEIGHT, PIXEL_HALFHEIGHT
+                )],
+                AddressA[calc_pkg::num_pixelcolorselect_bits(BYTES_PER_PIXEL)-1:0]
             };
 
-            wire we_lane_c = ClockEnA & WrA & (lane_idx_from_addr == i[_NUM_STRUCTURE_BITS-1:0]);
+            wire we_lane_c = ClockEnA & WrA & (lane_idx_from_addr == i[calc_pkg::num_structure_bits(
+                PIXEL_WIDTH, PIXEL_HEIGHT, BYTES_PER_PIXEL, PIXEL_HALFHEIGHT
+            )-1:0]);
             (* keep = "true" *) reg we_lane_q;
-            (* keep = "true" *) reg [_NUM_ADDRESS_B_BITS-1:0] addra_q;
-            (* keep = "true" *) reg [_NUM_DATA_A_BITS-1:0] dia_q;
+            (* keep = "true" *) reg [calc_pkg::num_address_b_bits(PIXEL_WIDTH, PIXEL_HALFHEIGHT)-1:0] addra_q;
+            (* keep = "true" *) reg [calc_pkg::num_data_a_bits()-1:0] dia_q;
 
             always @(posedge ClockA) begin
                 we_lane_q <= we_lane_c;
-                addra_q   <= AddressA[(_NUM_ADDRESS_A_BITS-_NUM_SUBPANELSELECT_BITS)-1-:_NUM_ADDRESS_B_BITS];
-                dia_q     <= DataInA;
+                addra_q <= AddressA[(calc_pkg::num_address_a_bits(
+                    PIXEL_WIDTH, PIXEL_HEIGHT, BYTES_PER_PIXEL, PIXEL_HALFHEIGHT
+                )-calc_pkg::num_subpanelselect_bits(
+                    PIXEL_HEIGHT, PIXEL_HALFHEIGHT
+                ))-1-:calc_pkg::num_address_b_bits(
+                    PIXEL_WIDTH, PIXEL_HALFHEIGHT
+                )];
+                dia_q <= DataInA;
             end
 
             mem_lane #(
-                .ADDR_BITS(_NUM_ADDRESS_B_BITS),
-                .DW(_NUM_DATA_A_BITS)
+                .ADDR_BITS(calc_pkg::num_address_b_bits(PIXEL_WIDTH, PIXEL_HALFHEIGHT)),
+                .DW(calc_pkg::num_data_a_bits())
             ) u_lane (
                 .clka (ClockA),
                 .ena  (1'b1),
@@ -77,7 +99,7 @@ module multimem #(
                 .enb  (ClockEnB),
                 .rstb (ResetA || ResetB),
                 .addrb(AddressB),
-                .dob  (qb_lanes_w[i*_NUM_DATA_A_BITS+:_NUM_DATA_A_BITS])
+                .dob  (qb_lanes_w[i*calc_pkg::num_data_a_bits()+:calc_pkg::num_data_a_bits()])
             );
         end
     endgenerate
