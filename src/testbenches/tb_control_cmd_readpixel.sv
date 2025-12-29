86edfc1d494ea07b5bfc5f6ee967af3679ee4270
Aaron White <w531t4@gmail.com>
2025-12-29 14:29:26 +0000

    substitute params_pkg for module params: tb_control_cmd_readpixel

--- src/testbenches/tb_control_cmd_readpixel.sv
+++ src/testbenches/tb_control_cmd_readpixel.sv
@@ -48,9 +48,9 @@ module tb_control_cmd_readpixel #(
     );
 
     control_cmd_readpixel #(
-        .BYTES_PER_PIXEL(params_pkg::BYTES_PER_PIXEL),
-        .PIXEL_HEIGHT(params_pkg::PIXEL_HEIGHT),
-        .PIXEL_WIDTH(params_pkg::PIXEL_WIDTH),
+        .BYTES_PER_PIXEL(BYTES_PER_PIXEL),
+        .PIXEL_HEIGHT(PIXEL_HEIGHT),
+        .PIXEL_WIDTH(PIXEL_WIDTH),
         ._UNUSED('d0)
     ) cmd_readpixel (
         .reset(reset),
