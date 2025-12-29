1ffb166fe3a5483b5ce7701b642902a7ddfe0b69
Aaron White <w531t4@gmail.com>
2025-12-29 18:56:33 +0000

    lint: fix unused/missing signal tb_fm6126a

--- src/testbenches/tb_fm6126init.sv
+++ src/testbenches/tb_fm6126init.sv
@@ -8,10 +8,11 @@ module tb_fm6126init;
     logic clk_root;
     logic reset;
     wire mask_en;
-    wire output_enable_out;
     wire [2:0] rgb1_out;
     wire [2:0] rgb2_out;
     wire latch_out;
+    wire pixclock_out;
+    wire reset_notify;
     always #5 clk_root <= ~clk_root;
     initial begin
 `ifdef DUMP_FILE_NAME
@@ -30,6 +31,18 @@ module tb_fm6126init;
         .mask_en(mask_en),
         .rgb1_out(rgb1_out),
         .rgb2_out(rgb2_out),
+        .pixclock_out(pixclock_out),
+        .reset_notify(reset_notify),
         .latch_out(latch_out)
     );
+    // verilog_format: off
+    wire _unused_ok = &{1'b0,
+                        mask_en,
+                        rgb1_out,
+                        rgb2_out,
+                        latch_out,
+                        pixclock_out,
+                        reset_notify,
+                        1'b0};
+    // verilog_format: on
 endmodule
