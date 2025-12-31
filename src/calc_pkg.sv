cdb6b6698157f55180c5f815fe92642887e4da83
Aaron White <w531t4@gmail.com>
2025-12-31 18:39:38 +0000

    whitespace changes to calc_pkg

--- src/calc_pkg.sv
+++ src/calc_pkg.sv
@@ -30,16 +30,15 @@ package calc_pkg;
     function automatic int unsigned num_address_a_bits(input int unsigned pixel_width, input int unsigned pixel_height,
                                                        input int unsigned bytes_per_pixel,
                                                        input int unsigned pixel_halfheight);
-        num_address_a_bits = num_subpanelselect_bits(pixel_height, pixel_halfheight)
-            + num_pixelcolorselect_bits(bytes_per_pixel)
-            + num_address_b_bits(pixel_width, pixel_halfheight);
+        num_address_a_bits = num_subpanelselect_bits(pixel_height, pixel_halfheight) +
+            num_pixelcolorselect_bits(bytes_per_pixel) + num_address_b_bits(pixel_width, pixel_halfheight);
     endfunction
 
     function automatic int unsigned num_structure_bits(input int unsigned pixel_width, input int unsigned pixel_height,
                                                        input int unsigned bytes_per_pixel,
                                                        input int unsigned pixel_halfheight);
-        num_structure_bits = num_address_a_bits(pixel_width, pixel_height, bytes_per_pixel, pixel_halfheight)
-            - num_address_b_bits(pixel_width, pixel_halfheight);
+        num_structure_bits = num_address_a_bits(pixel_width, pixel_height, bytes_per_pixel, pixel_halfheight) -
+            num_address_b_bits(pixel_width, pixel_halfheight);
     endfunction
 
     function automatic int unsigned num_data_a_bits();
@@ -48,13 +47,13 @@ package calc_pkg;
 
     function automatic int unsigned num_data_b_bits(input int unsigned pixel_height, input int unsigned bytes_per_pixel,
                                                     input int unsigned pixel_halfheight);
-        num_data_b_bits = ((1 << num_pixelcolorselect_bits(bytes_per_pixel))
-            << num_subpanelselect_bits(pixel_height, pixel_halfheight)) << $clog2(8);
+        num_data_b_bits = ((1 << num_pixelcolorselect_bits(bytes_per_pixel)) <<
+                           num_subpanelselect_bits(pixel_height, pixel_halfheight)) << $clog2(8);
     endfunction
 
     function automatic int unsigned num_bits_per_subpanel(
         input int unsigned pixel_height, input int unsigned bytes_per_pixel, input int unsigned pixel_halfheight);
-        num_bits_per_subpanel = num_data_b_bits(pixel_height, bytes_per_pixel, pixel_halfheight)
-            >> num_subpanelselect_bits(pixel_height, pixel_halfheight);
+        num_bits_per_subpanel = num_data_b_bits(pixel_height, bytes_per_pixel, pixel_halfheight) >>
+            num_subpanelselect_bits(pixel_height, pixel_halfheight);
     endfunction
 endpackage
