e139a26a8c37d45b196add0dd942b640bf107678
Aaron White <w531t4@gmail.com>
2026-01-02 17:27:23 +0000

    === begin inconsistent param state (move structures to types)

--- src/include/row4.svh
+++ src/include/row4.svh
@@ -3,95 +3,110 @@
 `include "sim_data.svh"
 `ifdef RGB24
 `ifdef W128
-localparam readrow_cmd_t cmd_readrow = readrow_cmd_t'({
-    cmd::READROW, row_addr_view_t'('h04), row_data_t'('h`W384_RGB24_ROW_HEX)
+localparam types::readrow_cmd_t cmd_readrow = types::readrow_cmd_t'({
+    cmd::READROW, types::row_addr_view_t'('h04), types::row_data_t'('h`W384_RGB24_ROW_HEX)
 });
 `else  // W128
-localparam readrow_cmd_t cmd_readrow = readrow_cmd_t'({
-    cmd::READROW, row_addr_view_t'('h04), row_data_t'('h`W64_RGB24_ROW_HEX)
+localparam types::readrow_cmd_t cmd_readrow = types::readrow_cmd_t'({
+    cmd::READROW, types::row_addr_view_t'('h04), types::row_data_t'('h`W64_RGB24_ROW_HEX)
 });
 
 `endif  // W128
 `else  // RGB24
 `ifdef W128
-localparam readrow_cmd_t cmd_readrow = readrow_cmd_t'({
-    cmd::READROW, row_addr_view_t'('h04), row_data_t'('h`W384_RGB565_ROW_HEX)
+localparam types::readrow_cmd_t cmd_readrow = types::readrow_cmd_t'({
+    cmd::READROW, types::row_addr_view_t'('h04), types::row_data_t'('h`W384_RGB565_ROW_HEX)
 });
 
 `else  // W128
-localparam readrow_cmd_t cmd_readrow = readrow_cmd_t'({
-    cmd::READROW, row_addr_view_t'('h04), row_data_t'('h`W64_RGB565_ROW_HEX)
+localparam types::readrow_cmd_t cmd_readrow = types::readrow_cmd_t'({
+    cmd::READROW, types::row_addr_view_t'('h04), types::row_data_t'('h`W64_RGB565_ROW_HEX)
 });
 `endif  // W128
 `endif  // RGB24
 // Add tests for pixel set command
-localparam brightness3_cmd_t cmd_brightness_1 = brightness3_cmd_t'({cmd::BRIGHTNESS_THREE});
+localparam types::brightness3_cmd_t cmd_brightness_1 = types::brightness3_cmd_t'({cmd::BRIGHTNESS_THREE});
 
-localparam readbrightness_cmd_t cmd_brightness_2 = readbrightness_cmd_t'({
-    cmd::READBRIGHTNESS, brightness_level_view_t'('h23)
+localparam types::readbrightness_cmd_t cmd_brightness_2 = types::readbrightness_cmd_t'({
+    cmd::READBRIGHTNESS, types::brightness_level_view_t'('h23)
 });  // "T" + \x23
-localparam readbrightness_cmd_t cmd_brightness_3 = readbrightness_cmd_t'({
-    cmd::READBRIGHTNESS, brightness_level_view_t'('h38)
+localparam types::readbrightness_cmd_t cmd_brightness_3 = types::readbrightness_cmd_t'({
+    cmd::READBRIGHTNESS, types::brightness_level_view_t'('h38)
 });  // "T" + \x38
 
-localparam blankpanel_cmd_t cmd_blankpanel = blankpanel_cmd_t'({cmd::BLANKPANEL});
+localparam types::blankpanel_cmd_t cmd_blankpanel = types::blankpanel_cmd_t'({cmd::BLANKPANEL});
 
 `ifdef USE_WATCHDOG
-localparam watchdog_cmd_t cmd_watchdog = watchdog_cmd_t'({
-    cmd::WATCHDOG, watchdog_pattern_t'(params::WATCHDOG_SIGNATURE_PATTERN)
+localparam types::watchdog_cmd_t cmd_watchdog = types::watchdog_cmd_t'({
+    cmd::WATCHDOG, types::watchdog_pattern_t'(params::WATCHDOG_SIGNATURE_PATTERN)
 });  // "W" + "DEADBEEFFEEBDAED"
 `endif  // USE_WATCHDOG
 `ifdef RGB24
 `ifdef W128
-localparam readpixel_cmd_t cmd_pixel_1 = readpixel_cmd_t'({
-    cmd::READPIXEL, row_addr_view_t'('h00), col_addr_view_t'('h0078), color_t'('h132040)
+localparam types::readpixel_cmd_t cmd_pixel_1 = types::readpixel_cmd_t'({
+    cmd::READPIXEL, types::row_addr_view_t'('h00), types::col_addr_view_t'('h0078), types::color_t'('h132040)
 });
-localparam readpixel_cmd_t cmd_pixel_2 = readpixel_cmd_t'({
-    cmd::READPIXEL, row_addr_view_t'('h01), col_addr_view_t'('h0079), color_t'('h304013)
+localparam types::readpixel_cmd_t cmd_pixel_2 = types::readpixel_cmd_t'({
+    cmd::READPIXEL, types::row_addr_view_t'('h01), types::col_addr_view_t'('h0079), types::color_t'('h304013)
 });
-localparam fillrect_cmd_t cmd_fillrect = fillrect_cmd_t'({
+localparam types::fillrect_cmd_t cmd_fillrect = types::fillrect_cmd_t'({
     cmd::FILLRECT,
-    col_field_t'('h0001),
-    row_field_t'('h0A),
-    col_field_t'('h0071),
-    row_field_t'('h05),
-    color_t'('hE0A932)
+    types::col_field_t'('h0001),
+    types::row_field_t'('h0A),
+    types::col_field_t'('h0071),
+    types::row_field_t'('h05),
+    types::color_t'('hE0A932)
 });
 `else  // W128
-localparam readpixel_cmd_t cmd_pixel_1 = readpixel_cmd_t'({
-    cmd::READPIXEL, row_addr_view_t'('h00), col_addr_view_t'('h30), color_t'('h132040)
+localparam types::readpixel_cmd_t cmd_pixel_1 = types::readpixel_cmd_t'({
+    cmd::READPIXEL, types::row_addr_view_t'('h00), types::col_addr_view_t'('h30), types::color_t'('h132040)
 });
-localparam readpixel_cmd_t cmd_pixel_2 = readpixel_cmd_t'({
-    cmd::READPIXEL, row_addr_view_t'('h01), col_addr_view_t'('h32), color_t'('h304013)
+localparam types::readpixel_cmd_t cmd_pixel_2 = types::readpixel_cmd_t'({
+    cmd::READPIXEL, types::row_addr_view_t'('h01), types::col_addr_view_t'('h32), types::color_t'('h304013)
 });
-localparam fillrect_cmd_t cmd_fillrect = fillrect_cmd_t'({
-    cmd::FILLRECT, col_field_t'('h05), row_field_t'('h0A), col_field_t'('h10), row_field_t'('h05), color_t'('hE0A932)
+localparam types::fillrect_cmd_t cmd_fillrect = types::fillrect_cmd_t'({
+    cmd::FILLRECT,
+    types::col_field_t'('h05),
+    types::row_field_t'('h0A),
+    types::col_field_t'('h10),
+    types::row_field_t'('h05),
+    types::color_t'('hE0A932)
 });
 `endif  // W128
-localparam fillpanel_cmd_t cmd_fillpanel = fillpanel_cmd_t'({cmd::FILLPANEL, color_t'('h314287)});
+localparam types::fillpanel_cmd_t cmd_fillpanel = types::fillpanel_cmd_t'({cmd::FILLPANEL, types::color_t'('h314287)});
 `else  // RGB24
 `ifdef W128
-localparam readpixel_cmd_t cmd_pixel_1 = readpixel_cmd_t'({
-    cmd::READPIXEL, row_addr_view_t'('h00), col_addr_view_t'('h0078), color_t'('h1020)
+localparam types::readpixel_cmd_t cmd_pixel_1 = types::readpixel_cmd_t'({
+    cmd::READPIXEL, types::row_addr_view_t'('h00), types::col_addr_view_t'('h0078), types::color_t'('h1020)
 });
-localparam readpixel_cmd_t cmd_pixel_2 = readpixel_cmd_t'({
-    cmd::READPIXEL, row_addr_view_t'('h01), col_addr_view_t'('h0079), color_t'('h3040)
+localparam types::readpixel_cmd_t cmd_pixel_2 = types::readpixel_cmd_t'({
+    cmd::READPIXEL, types::row_addr_view_t'('h01), types::col_addr_view_t'('h0079), types::color_t'('h3040)
 });
-localparam fillrect_cmd_t cmd_fillrect = fillrect_cmd_t'({
-    cmd::FILLRECT, col_field_t'('h0001), row_field_t'('h0A), col_field_t'('h0071), row_field_t'('h05), color_t'('hE0A9)
+localparam types::fillrect_cmd_t cmd_fillrect = types::fillrect_cmd_t'({
+    cmd::FILLRECT,
+    types::col_field_t'('h0001),
+    types::row_field_t'('h0A),
+    types::col_field_t'('h0071),
+    types::row_field_t'('h05),
+    types::color_t'('hE0A9)
 });
 `else  // W128
-localparam readpixel_cmd_t cmd_pixel_1 = readpixel_cmd_t'({
-    cmd::READPIXEL, row_addr_view_t'('h00), col_addr_view_t'('h30), color_t'('h1020)
+localparam types::readpixel_cmd_t cmd_pixel_1 = types::readpixel_cmd_t'({
+    cmd::READPIXEL, types::row_addr_view_t'('h00), types::col_addr_view_t'('h30), types::color_t'('h1020)
 });
-localparam readpixel_cmd_t cmd_pixel_2 = readpixel_cmd_t'({
-    cmd::READPIXEL, row_addr_view_t'('h01), col_addr_view_t'('h32), color_t'('h3040)
+localparam types::readpixel_cmd_t cmd_pixel_2 = types::readpixel_cmd_t'({
+    cmd::READPIXEL, types::row_addr_view_t'('h01), types::col_addr_view_t'('h32), types::color_t'('h3040)
 });
-localparam fillrect_cmd_t cmd_fillrect = fillrect_cmd_t'({
-    cmd::FILLRECT, col_field_t'('h05), row_field_t'('h0A), col_field_t'('h10), row_field_t'('h05), color_t'('hE0A9)
+localparam types::fillrect_cmd_t cmd_fillrect = types::fillrect_cmd_t'({
+    cmd::FILLRECT,
+    types::col_field_t'('h05),
+    types::row_field_t'('h0A),
+    types::col_field_t'('h10),
+    types::row_field_t'('h05),
+    types::color_t'('hE0A9)
 });
 `endif  // W128
-localparam fillpanel_cmd_t cmd_fillpanel = fillpanel_cmd_t'({cmd::FILLPANEL, color_t'('h3142)});
+localparam types::fillpanel_cmd_t cmd_fillpanel = types::fillpanel_cmd_t'({cmd::FILLPANEL, types::color_t'('h3142)});
 `endif  // RGB24
 
 `define CMD_SERIES_FIELDS \
--- src/testbenches/tb_control_cmd_readpixel.sv
+++ src/testbenches/tb_control_cmd_readpixel.sv
@@ -10,15 +10,13 @@ module tb_control_cmd_readpixel #(
     parameter integer unsigned BYTES_PER_PIXEL = params::BYTES_PER_PIXEL,
     parameter integer unsigned PIXEL_HEIGHT = params::PIXEL_HEIGHT,
     parameter integer unsigned PIXEL_WIDTH = params::PIXEL_WIDTH,
-    parameter integer unsigned BRIGHTNESS_LEVELS = params::BRIGHTNESS_LEVELS,
     parameter real SIM_HALF_PERIOD_NS = params::SIM_HALF_PERIOD_NS,
     // verilator lint_off UNUSEDPARAM
     parameter integer unsigned _UNUSED = 0
     // verilator lint_on UNUSEDPARAM
 );
-    `include "structures.svh"
     `include "row4.svh"
-    localparam integer unsigned command_without_opcode_bits = $bits(readpixel_cmd_t) - $bits(cmd::opcode_t);
+    localparam integer unsigned command_without_opcode_bits = $bits(types::readpixel_cmd_t) - $bits(cmd::opcode_t);
     localparam logic [command_without_opcode_bits-1:0] cmd_pixel_1_local = cmd_pixel_1[command_without_opcode_bits-1:0];
     localparam int unsigned STREAM_BYTES = ($bits(cmd_pixel_1_local) / 8);
     localparam int ROW_IDX = 0;
