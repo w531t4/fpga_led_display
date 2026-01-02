2e0f72833b83548af76878603921c9c590b0c4a7
Aaron White <w531t4@gmail.com>
2026-01-02 23:23:27 +0000

    lint: disable UNUSEDPARAM for param pkg

--- src/params.sv
+++ src/params.sv
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2025 Aaron White <w531t4@gmail.com>
 // SPDX-License-Identifier: MIT
 package params;
+    // verilator lint_off UNUSEDPARAM
 `ifdef CLK_110
     parameter int unsigned ROOT_CLOCK = 110_000_000;
     parameter int unsigned PLL_SPEED = 4;
@@ -19,26 +20,21 @@ package params;
 `endif
     // !SPI
     // Use this to determine what baudrate to require at ctrl/rx_in
-    // verilator lint_off UNUSEDPARAM
     parameter int unsigned CTRLR_UART_RX_FREQ_GOAL = 244444;
     parameter int unsigned CTRLR_CLK_TICKS_PER_BIT = $rtoi(ROOT_CLOCK / CTRLR_UART_RX_FREQ_GOAL * 1.0);
-    // verilator lint_on UNUSEDPARAM
 
     // DEBUGGER
     // Describes the baudrate for sending messages to debugger client
-    // verilator lint_off UNUSEDPARAM
     parameter int unsigned DEBUG_UART_TX_FREQ_GOAL = 115200;
     parameter int unsigned DEBUG_TX_UART_TICKS_PER_BIT = ROOT_CLOCK / DEBUG_UART_TX_FREQ_GOAL;
     parameter int unsigned DEBUG_UART_RX_FREQ_GOAL = 244444;
     parameter int unsigned DEBUG_MSGS_PER_SEC_TICKS = ROOT_CLOCK / DEBUG_UART_RX_FREQ_GOAL;
-    // verilator lint_on UNUSEDPARAM
 
     // Use this to tune what clock freq we expose to matrix_scan
     parameter int unsigned DIVIDE_CLK_BY_X_FOR_MATRIX = 2;
 
     // USE_WATCHDOG
     // reset control logic if watchdog isn't satisfied within x seconds
-    // verilator lint_off UNUSEDPARAM
     parameter real WATCHDOG_CONTROL_FREQ_GOAL = 0.1;  // 10 seconds
 `ifdef WATCHDOG_CONTROL_TICKS
     parameter int unsigned WATCHDOG_CONTROL_TICKS = `WATCHDOG_CONTROL_TICKS;
@@ -47,22 +43,17 @@ package params;
 `endif
     parameter int unsigned WATCHDOG_SIGNATURE_BITS = 64;
     parameter logic [WATCHDOG_SIGNATURE_BITS-1:0] WATCHDOG_SIGNATURE_PATTERN = 64'hDEADBEEFFEEBDAED;
-    // verilator lint_on UNUSEDPARAM
 
     // SIM
-    // verilator lint_off UNUSEDPARAM
 `ifdef SIM_HALF_PERIOD_NS
     parameter real SIM_HALF_PERIOD_NS = `SIM_HALF_PERIOD_NS;
 `else
     parameter real SIM_HALF_PERIOD_NS = ((1.0 / ROOT_CLOCK) * 1000000000) / 2.0;
 `endif
-    // verilator lint_on UNUSEDPARAM
 
     // SIM !SPI
     // Use smaller value in testbench so we don't infinitely sim.
-    // verilator lint_off UNUSEDPARAM
     parameter int unsigned DEBUG_MSGS_PER_SEC_TICKS_SIM = 'd15;
-    // verilator lint_on UNUSEDPARAM
 
 `ifdef RGB24
     parameter int unsigned _BYTES_PER_PIXEL = 3;
@@ -113,4 +104,5 @@ package params;
 
     parameter integer BRIGHTNESS_BASE_TIMEOUT = 10;
     parameter integer BRIGHTNESS_STATE_TIMEOUT_OVERLAP = 'd67;
+    // verilator lint_on UNUSEDPARAM
 endpackage
