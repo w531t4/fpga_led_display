a5c7e18c6619b5c1e218a3c09e82b4a7ce3db241
Aaron White <w531t4@gmail.com>
2026-01-03 18:27:42 +0000

    remove more of PIXEL_HEIGHT

--- src/testbenches/tb_control_cmd_fillpanel.sv
+++ src/testbenches/tb_control_cmd_fillpanel.sv
@@ -10,10 +10,9 @@
 // and returns to idle after done.
 module tb_control_cmd_fillpanel;
     localparam int unsigned BYTES_PER_PIXEL = params::BYTES_PER_PIXEL;
-    localparam int unsigned PIXEL_HEIGHT = params::PIXEL_HEIGHT;
     localparam real SIM_HALF_PERIOD_NS = 1.0;
     localparam int MEM_NUM_BYTES = (1 << $bits(types::mem_write_addr_t));
-    localparam int TOTAL_WRITES = params::PIXEL_WIDTH * PIXEL_HEIGHT * BYTES_PER_PIXEL;
+    localparam int TOTAL_WRITES = params::PIXEL_WIDTH * params::PIXEL_HEIGHT * BYTES_PER_PIXEL;
     localparam types::color_t COLOR = 16'hCAFE;
 
     // === Testbench scaffolding ===
@@ -35,7 +34,6 @@ module tb_control_cmd_fillpanel;
     // === DUT wiring ===
     control_cmd_fillpanel #(
         .BYTES_PER_PIXEL(BYTES_PER_PIXEL),
-        .PIXEL_HEIGHT(PIXEL_HEIGHT),
         ._UNUSED('d0)
     ) dut (
         .reset(reset),
