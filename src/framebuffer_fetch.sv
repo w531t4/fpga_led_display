4255d4b7e257d918405c72d15eac90e673634dab
Aaron White <w531t4@gmail.com>
2026-01-08 00:23:44 +0000

    update notes in fbf - mirror column address

--- src/framebuffer_fetch.sv
+++ src/framebuffer_fetch.sv
@@ -29,7 +29,19 @@ module framebuffer_fetch #(
     assign pixel_load_counter2[3:0] = {2'b0, pixel_load_counter[1:0]};
 `endif
 
-    assign ram_address = {row_address, column_address};
+    // When we write data... i = 0,  1,  2,      n
+    //                  command{D0, D1, D2, ... Dn) ...
+    // ... will be written to memory mem = {d[n], ..., d[2], d[1], d[0]}
+    //  - We're writing the memory like this because it's WAY EASIER to do so... can take advantage of struct.bytes[i]
+    //  - We'll align our coordinate plane with ESPHome - such that (0,0) describes the upper left hand corner of a display.
+    // Since our data is written like this
+    //      memory mem = {d[n], ..., d[2], d[1], d[0]}
+    // We calculate the inverse of the column address to produce
+    //      {d[0], d[1], d[2], ... d[n]}
+    //          [ 0 --> n]
+    //         left -> right
+    wire types::col_addr_t column_address_mirrored = types::col_addr_t'(params::PIXEL_WIDTH - 1) - column_address;
+    assign ram_address = {row_address, column_address_mirrored};
 
     timeout #(
         .COUNTER_WIDTH(2)
