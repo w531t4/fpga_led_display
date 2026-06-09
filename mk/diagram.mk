7326d21d7abd2ad33411bc7d730b5a3ff19d5214
Aaron White <w531t4@gmail.com>
2026-06-09 13:11:39 +0000

    fix: loopviz takes long time to viz, and contains non-usable information

--- mk/diagram.mk
+++ mk/diagram.mk
@@ -1,28 +1,8 @@
 # SPDX-FileCopyrightText: 2025 Aaron White <w531t4@gmail.com>
 # SPDX-License-Identifier: MIT
 
-loopviz: $(ARTIFACT_DIR)/mydesign_show.svg ## Render Yosys graphviz loop visualization
-$(ARTIFACT_DIR)/mydesign_show.svg: $(ARTIFACT_DIR)/mydesign_show.dot | $(ARTIFACT_DIR)
-	$(TOOLPATH)/dot -Kdot -o $@ -Tsvg $<
-
-loopviz_pre: $(ARTIFACT_DIR)/mydesign_show_pre.svg ## Render pre-optimization Yosys graphviz loop visualization
-$(ARTIFACT_DIR)/mydesign_show_pre.svg: $(ARTIFACT_DIR)/mydesign_show_pre.dot | $(ARTIFACT_DIR)
-	$(TOOLPATH)/dot -Kdot -o $@ -Tsvg $<
-$(ARTIFACT_DIR)/mydesign_vizclean.json: $(ARTIFACT_DIR)/mydesign.json | $(ARTIFACT_DIR)
-	jq 'del(.modules.BB, .modules.BBPU, .modules.BBPD, .modules.TRELLIS_IO)' $< > $@
-
-$(ARTIFACT_DIR)/netlist.svg: $(ARTIFACT_DIR)/mydesign_vizclean.json | $(ARTIFACT_DIR)
+$(ARTIFACT_DIR)/netlist_hier.svg: $(ARTIFACT_DIR)/mydesign_hier.json | $(ARTIFACT_DIR)
 	$(NETLISTSVG) $< -o $@
 
-ifeq ($(YOSYS_INCLUDE_EXTRA),true)
-$(ARTIFACT_DIR)/mydesign_pre_vizclean.json: $(ARTIFACT_DIR)/mydesign_pre.json | $(ARTIFACT_DIR)
-	jq 'del(.modules.BB, .modules.BBPU, .modules.BBPD, .modules.TRELLIS_IO)' $< > $@
-
-$(ARTIFACT_DIR)/netlist_pre.svg: $(ARTIFACT_DIR)/mydesign_pre_vizclean.json | $(ARTIFACT_DIR)
-	$(NETLISTSVG) $< -o $@
-endif
-
-DIAGRAM_TARGETS:=$(ARTIFACT_DIR)/netlist.svg
-DIAGRAM_TARGETS += $(if $(filter true,$(YOSYS_INCLUDE_EXTRA)),$(ARTIFACT_DIR)/netlist_pre.svg)
-
-diagram: $(DIAGRAM_TARGETS) ## Render netlistsvg diagrams
+diagram_hier: $(ARTIFACT_DIR)/netlist_hier.svg
+diagram: diagram_hier ## Render default usable netlistsvg diagram
